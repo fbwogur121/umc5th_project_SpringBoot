74c00aa6640daf154903f928fd5c80a98a9c43af
fbwogur121 <fbwogur121@naver.com>
2024-02-13 14:11:34 +0900

    ..

--- src/main/java/umc/study/service/StoreService/StoreCommandService.java
+++
@@ -1,9 +0,0 @@
-package umc.study.service.StoreService;
-
-import umc.study.domain.Review;
-import umc.study.web.dto.StoreRequestDTO;
-
-public interface StoreCommandService {
-
-    Review createReview(Long memberId, Long storeId, StoreRequestDTO.ReveiwDTO request);
-}
